863670f8f7cab78b4e9dd60d62c73c3c0a48b55e
jean.prouvay@gmail.com <jean.prouvay@gmail.com>
2020-01-16 14:54:18 -0300

    Implementacion de Action Sheet

--- 03-componentes/src/app/app.module.ts
+++ 03-componentes/src/app/app.module.ts
@@ -10,12 +10,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ComponentsModule } from './components/components.module';
 
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
-    BrowserModule, 
-    IonicModule.forRoot(), 
+    BrowserModule,
+    IonicModule.forRoot(),
     AppRoutingModule,
     ComponentsModule
   ],
