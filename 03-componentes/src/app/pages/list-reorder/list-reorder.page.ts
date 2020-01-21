9cfa751d5dfba5fa4b443c45d297a66845626089
jean.prouvay@gmail.com <jean.prouvay@gmail.com>
2020-01-21 16:03:29 -0300

    termino list reorder

--- 03-componentes/src/app/pages/list-reorder/list-reorder.page.ts
+++ 03-componentes/src/app/pages/list-reorder/list-reorder.page.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListReorderPage implements OnInit {
 
+  personajes = ['Ironman','Spiderman','Venom','DeadPool'];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  reorder(evento){
+    const itemMover = this.personajes.splice(evento.detail.from,1)[0];
+    this.personajes.splice(evento.detail.to, 0, itemMover);
+    evento.detail.complete();
+  }
+
+  onClick(){
+    console.log(this.personajes);
   }
 
 }
