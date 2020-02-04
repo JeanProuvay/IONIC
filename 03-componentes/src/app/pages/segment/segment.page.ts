b2326106c0f313f9f226e86c8e1ea8f33fdc1b27
Jean Michel Prouvay Hernandez <jean.prouvay@gmail.com>
2020-02-04 00:13:17 -0300

    segment terminado

--- 03-componentes/src/app/pages/segment/segment.page.ts
+++ 03-componentes/src/app/pages/segment/segment.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
+import { DataService } from 'src/app/services/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-segment',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SegmentPage implements OnInit {
 
-  constructor() { }
+  @ViewChild('segmento', {static: false}) lista: IonSegment;
+  superHeroes: Observable<any>;
+  publisher: string = '';
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.superHeroes = this.dataService.getHeroes();
+    console.log(this.superHeroes);
+  }
+
+  segmentChanged(evento){
+
+
+    if( evento.detail.value === 'todos' ){
+      this.publisher = '';
+      return;
+    }
+    this.publisher = evento.detail.value;
   }
 
 }
